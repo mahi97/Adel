c27f660f23d8502b7b4ada2c07e4c7999e234700
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2019-09-15 23:04:09 +0430

    add event message

--- Messages/event.py
+++ Messages/event.py
@@ -1,6 +1,8 @@
 class Event:
   def __init__(self):
     self.event = 0
-    self.winner = 0 # 0 -> A , 1 -> B
-    self.ballout = False
+    self.team = 0 # 0 -> A , 1 -> B
+    self.player = -1
+    self.qualifier = 'NONE'
+    self.ball_out = False
     self.game_reset = False
